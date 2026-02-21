e4c9f7aeb115ad3236cfff6b3c9d9043431003b8
Kev-cipher <kevin.j4110@gmail.com>
2026-02-21 10:15:09 -0500

    testing

--- recipe.js
+++ recipe.js
@@ -2,8 +2,8 @@ const slideshow3 = document.getElementById('slideshow3');
 const recipeBox = document.createElement('div');
 recipeBox.classList.add('recipe1')
 recipeBox.innerHTML = `
-    <h4>${recipeName}</h4>
-    <p>${ingredients}</p>
+    <h4>Recipe 1</h4>
+    <p>pizza</p>
 `;
 slideshow3.appendChild('recipeBox');
 
