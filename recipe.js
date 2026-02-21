9f22aec27f053ced06eada67061c83d504f4fa2f
Kev-cipher <kevin.j4110@gmail.com>
2026-02-21 10:12:48 -0500

    Testing

--- recipe.js
+++ recipe.js
@@ -1,3 +1,9 @@
-const word = document.createElement("p");
-word.innerHTML = 'this is where new box should appear'
-document.getElementById('slideshow3').appendChild('word');
+const slideshow3 = document.getElementById('slideshow3');
+const recipeBox = document.createElement('div');
+recipeBox.classList.add('recipe1')
+recipeBox.innerHTML = `
+    <h4>${recipeName}</h4>
+    <p>${ingredients}</p>
+`;
+slideshow3.appendChild('recipeBox');
+
