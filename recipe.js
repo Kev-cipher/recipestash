38a3934bc9e3fa99bd8698f24efc3c17e503f21d
Samuel Joseph <samuelejoseph18@gmail.com>
2026-04-24 14:03:29 -0400

    Implement recipe saving feature with image upload
    Added functionality to save a recipe with an image, name, description, and category.

--- recipe.js
+++ recipe.js
@@ -7,6 +7,9 @@ const save = document.getElementById("save")
 const exit = document.getElementById("exit")
 const addrecipe = document.getElementById("addRecipe")
 const popup = document.getElementById("recipeform")
+const createBox = document.getElementById("slideshow1")
+let reader = new FileReader();
+let pickedImage = null;
 
 recipeBox.classList.add("recipe1");
 recipeBox.innerHTML = `
@@ -37,5 +40,46 @@ darkMode.addEventListener("click", () => {
   document.body.classList.toggle("dark");
     updateIcon();
 });
+
+reader.onload = function(event) {
+pickedImage = reader.result
+}
+
+save.addEventListener("click", function(event) {
+
+const recipeName = document.getElementById("name").value
+const description = document.getElementById("desc").value
+const image = pickedImage
+const category = document.getElementById("category").value
+const recipe = document.createElement("div")
+
+if (!recipeName || !description || !category) {
+  return;
+}
+
+const boxRecipe = document.createElement("h2")
+const boxDesc = document.createElement("p");
+const boxCategory = document.createElement("span")
+const boxImage = document.createElement("img");
+
+boxRecipe.textContent = recipeName
+boxDesc.textContent = description
+boxImage.src = pickedImage
+boxCategory.textContent = category
+
+recipe.appendChild(boxImage)
+recipe.appendChild(boxRecipe);
+recipe.appendChild(boxCategory);
+recipe.appendChild(boxDesc);
+pickedImage = null;
+
+createBox.appendChild(recipe)
+
+popup.style.display = "none";
+document.getElementById("form").reset()
+event.preventDefault()
+
+});
+
 
 updateIcon() //makes sure only one icon shows when site is opened for the first time
