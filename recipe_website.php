23ea4be3ff6c0bf7064179b4567869a347b03f07
Kevin J <kevin.j4110@gmail.com>
2026-06-23 11:01:06 -0400

    Update recipe_website.php
    Updated the recipe website to the latest update from the recipe_website.html which includes dark mode and add recipe feature. Fixed indentation and added comments

--- recipe_website.php
+++ recipe_website.php
@@ -33,29 +33,79 @@ $isLoggedIn = isset($_SESSION["user_id"]);
         </div>
         
       <!--Second section-->
-      <div class="container2">
-        <ul id="headerLinks">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Favorites</a></li>
-          <li><a href="#">Trending</a></li>
-          <li><a href="#">Categories</a></li>
-          <li id="About"><a href="#">About Us</a></li>
-        </ul>
-        <hr>
-      </div>
-      <div>
-             <!--Side bar-->
-      <div class="sidebar">
-        <div id="sidetitle">
-            <h4>Popular</h4>
-        </div>
-        <hr>
-      </div>
-      <!--Third section-->
-      <div>
+       <div class="container2">
+                <ul id="headerLinks">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Favorites</a></li>
+                    <li><a href="#">Trending</a></li>
+                    <li><a href="#">Categories</a></li>
+                    <li id="About"><a href="#">About Us</a></li>
+                </ul>
+                <hr>
+            </div>
+            <!--Third Section-->
+            <div class="container3">
+                <div class="slideshows">
+                    <div id="slideshow1">
+                        <h3>Create Recipe
+                            <button id="addRecipe">
+                                <span>&#43;</span>
+                            </button>
+                            <hr>
+                        </h3>
+                    </div>
+                    <div id="slideshow2">
+                        <h3>Favorites</h3>
+                        <hr>
+                    </div>
+                    <div id="slideshow3">
+                        <h3>Recently Published</h3>
+                        <hr>
+                    </div>
+                </div>
+            <!--Side Bar Section-->
+                <div class="sidebar">
+                    <div id="sidetitle">
+                        <h4>Popular</h4>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+            <!--Recipe Form pop up for adding recipe into the given container-->  
+            <div class="recipeform" id="recipeform">
+                <form class="form-container">
+                    <h1>Recipe</h1>
 
-      </div>
-      </div>
-    </div>
-  </body>
+                    <label for="name"><b>Recipe name</b></label>
+                    <input placeholder="Enter recipe name" id="name">
+
+                    <label for="desc"><b>Description</b></label>
+                    <textarea placeholder="Give us the rundown..." id="desc" rows="4"></textarea>
+
+                    <label for="foodImage" class="image-upload">
+                        <b>Add an Image</b>
+                        <span>+</span>
+                        <input type="file" id="foodImage" accept="image/*">
+                    </label>
+
+                    <label for="type"><b>Category</b></label>
+                    <select id="category">
+                        <option value="">--Please choose a category</option>
+                        <option value="breakfast">Breakfast</option>
+                        <option value="lunch">Lunch</option>
+                        <option value="dinner">Dinner</option>
+                        <option value="quicksnack">Quick-Snack</option>
+                    </select>   
+
+                    <button id="save">Save</button>
+                    <button id="exit">
+                        <img src="images/disabled_by_default_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg">
+                    </button>
+                </form>
+            </div>
+
+        </div> 
+            <!-- Javascript link-->
+        <script src="recipe.js"></script>
+    </body>
 </html>
